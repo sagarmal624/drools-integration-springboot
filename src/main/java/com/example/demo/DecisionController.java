d2281ba3b9df41d3354ca29a0ba3043338ecba22
Sagar Shankhala <sagshank@publicisgroupe.net>
2021-05-06 09:26:45 +0530

    first comit

---
+++ src/main/java/com/example/demo/DecisionController.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DecisionController {
+
+    @Autowired
+    private KieContainer kieContainer;
+
+
+    @PostMapping("/discount")
+    private OrderRequest getDiscountPercent(@RequestBody OrderRequest orderRequest) {
+        KieSession kieSession = kieContainer.newKieSession();
+        kieSession.insert(orderRequest);
+        kieSession.fireAllRules();
+        kieSession.dispose();
+        return orderRequest;
+    }
+}
